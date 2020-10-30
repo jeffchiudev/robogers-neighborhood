56e08cc188f471054935cdb80071a51c29e2d181
Jeff Chiu <jwchiu82@gmail.com>
2020-10-30 12:12:46 -0700

    pass logic for single digits 1,2,3

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,13 @@ function beepBoop(value) {
   for (let i=0; i<=value; i++) {
     valueToArray.push(i);
   }
-  for (let j = 0; j <= valueToArray.length; j++) {
-    if (valueToArray[j] == 1) {
-      valueToArray[j] = "Beep!";
+  for (let i = 0; i <= valueToArray.length; i++) {
+    if (valueToArray[i] == 1) {
+      valueToArray[i] = "Beep!";
+    } else if (valueToArray[i] == 2) {
+      valueToArray[i] = "Boop"
+    } else if (valueToArray[i] == 3) {
+      valueToArray[i] = "Won't you be my neighbor"
     }
   }
   console.log(valueToArray);
---
+++ js/scripts.test.js
@@ -0,0 +1,25 @@
+Describe: beepBoop()
+
+//Test: "It returns an array of 0 if the number 0 is input"
+//Expect(beepBoop(0)).toEqual(0);
+
+//Test: "It returns an array of numbers if any number greater than 0 is input"
+//Expect(beepBoop(5)).toEqual(1,2,3,4,5);
+
+Test: "It returns a message if a non-number is input"
+Expect(beepBoop(@$%azAz)).toEqual("Sorry; Mr. Robogers only likes playing with numbers");
+
+//Test: "It returns a 'Beep!' if a value contains a 1"
+//Expect(beepBoop(1)).toEqual("Beep!");
+
+//Test: "It returns a 'Boop' if a value contains a 2"
+//Expect(beepBoop(2)).toEqual("Boop");
+
+//Test: "It returns 'Won't you be my neighbor?' if a value contains a 3"
+//Expect(beepBoop(3)).toEqual("Won't you be my neighbor?");
+
+Test: "It returns accurate logic for digits with 2+ digits"
+Expect(beepBoop(32 || 13 || 23)).toEqual("Won't you be my neighbor?");
+
+Test: ""
+Expect(beepBoop()).toEqual();
